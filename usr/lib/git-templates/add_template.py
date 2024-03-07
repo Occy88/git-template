bccf09268c06a2a9753cc2d6d341be4f7afdf624
Octavio <octavio@myrecovery.ai>
2024-03-07 21:30:29 +0000

    w

--- usr/lib/git-templates/add_template.py
+++ usr/lib/git-templates/add_template.py
@@ -1,7 +1,8 @@
 import sys
 import yaml
+import argparse
 from pathlib import Path
-
+from urllib.parse import urlparse
 
 def add_template(ref, url, branch=None):
     templates_path = Path('.git/templates/meta.yaml')
@@ -18,21 +19,33 @@ def add_template(ref, url, branch=None):
     templates[ref] = {'url': url, 'branch': branch}
 
     with open(templates_path, 'w') as file:
-        yaml.safe_dump(templates, file)
+        yaml.safe_dump(templates, file, default_flow_style=False)
 
+def get_repo_name_from_url(url):
+    """Extracts the repository name from a Git URL."""
+    path = urlparse(url).path
+    ref=path.split('/')[-1].replace('.git', '') if path else None
+    if not ref:
+        raise ValueError(f"Ref not found from '{path}', set it manually with `-r`")
+    return ref
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: python add_template.py <ref> <url> [<branch>]")
-        sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(description="Add a git template to the .git/templates/meta.yaml file.")
+    parser.add_argument('-r', '--ref', type=str, help="Reference name for the template. Defaults to the repo name extracted from the URL.")
+    parser.add_argument('url', type=str, help="URL of the git repository.")
+    parser.add_argument('-b', '--branch', type=str, help="Branch name. If not specified, defaults to null in the YAML file.", default=None)
+    parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit.')
 
-    _ref = sys.argv[1]
-    _url = sys.argv[2]
-    _branch = sys.argv[3] if len(sys.argv) > 3 else None
+    args = parser.parse_args()
+
+    ref = args.ref if args.ref else get_repo_name_from_url(args.url)
 
     try:
-        add_template(_ref, _url, _branch)
-        print(f"Template '{_ref}' added successfully.")
+        add_template(ref, args.url, args.branch)
+        print(f"Template '{ref}' added successfully.")
     except ValueError as e:
         print(e)
         sys.exit(1)
+
+if __name__ == '__main__':
+    main()
