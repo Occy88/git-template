d5c07218cda17e5a371c4827aeaaa5655b73ab97
Octavio <octavio@myrecovery.ai>
2024-03-08 11:15:12 +0000

    update

--- git_templates/main.py
+++ git_templates/main.py
@@ -1,5 +1,8 @@
 import sys
+
 from . import commands
+
+
 def main():
     if len(sys.argv) < 2 or sys.argv[1] not in commands.__all__:
         print(f"Usage: {', '.join(commands.__all__) }")
@@ -8,12 +11,12 @@ def main():
     command = sys.argv[1]
     args = sys.argv[2:]
     try:
-        cmd=getattr(commands,command)
+        cmd = getattr(commands, command)
         cmd(*args)
     except ImportError:
         print(f"Error: Command '{command}' is not supported.")
         return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
